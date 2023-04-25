4a29d7545339ae8983313e09cde63b2febbfe157
Guimbreon <gui.acs03@gmail.com>
2023-04-25 20:40:01 +0100

    Adding and moddifying the Script File

--- Script.R
+++ Script.R
@@ -30,24 +30,35 @@ for(i in 1:nrow(Grupo10))
   }else {
     Grupo10$HorasSono[i] = NA
   }
-
 #FIM LIMPEZA DE DADOS
 
-#PARTE 3
-TabelaCont1 = table(Grupo10$Sexo,Grupo10$Burnout_P1)
+#PARTE 1
+Dormem = unique(Grupo10$Idade, is.na=TRUE)
 
-TabelaCont1 = cbind(TabelaCont1)
+my_table <- data.frame(
+  Idade = numeric(),
+  HorasSono = numeric(),
+  stringsAsFactors = FALSE
+)
 
-rownames(TabelaCont1) = c("Female","Male")
+# Loop os valores de Dormem e analiza os dados referentes 
+#A cada idade no Grupo 10 
+# e faz a média da hora de sono
+for(i in na.omit(Dormem)) {
+  Sleep = na.omit(filter(Grupo10, Idade == i))
+  mean_sono <- mean(Sleep$HorasSono)
+  row <- data.frame(Idade = i, HorasSono = mean_sono)
+  my_table <- rbind(my_table, row)
+}
 
-
-barplot((TabelaCont1), beside = TRUE,col = c("green","purple"),
-        main = "Distribuição da Resposta da questão do Burnout 1",
-        ylim = c(0,25),
-        legend = c("Feminino","Masculino"))
-
+# View the resulting table
+View(my_table)
+my_table
 #PARTE 2
 #Comparação da média de Horas de estudo entre Homem
+
+if(!require(dplyr)) install.packege("dplyr")
+library(dplyr)
 
 Female_HorasEstudo = filter(Grupo10, Sexo == 1)
 
@@ -68,12 +79,17 @@ Diff_HorasEstudo = data.frame("Feminino-Masculino" = perc_incr_F_M,"Masculino-Fe
 View(Mean_HorasEstudo)  #Ver a média de Horas de Estudo
 View(Diff_HorasEstudo)  #Ver a diferença entre as Horas de estudo
 
-#PARTE 1
-Dormem = unique(Grupo10$Idade, is.na=TRUE)
+
+#PARTE 3
+TabelaCont1 = table(Grupo10$Sexo,Grupo10$Burnout_P1)
+
+TabelaCont1 = cbind(TabelaCont1)
+
+rownames(TabelaCont1) = c("Female","Male")
 
 
-View(Dormem)
+barplot((TabelaCont1), beside = TRUE,col = c("green","purple"),
+        main = "Distribuição da Resposta da questão do Burnout 1",
+        ylim = c(0,25),
+        legend = c("Feminino","Masculino"))
 
-#Listing all the ages without the value "missing"
-for(i in na.omit(Dormem))
-  print(i)
